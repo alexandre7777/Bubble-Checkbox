3ba0bf4167aadc3152a014c81e3e1d0c41cd9ca9
Alexandre <alexandre.genet7@gmail.com>
2018-06-28 14:10:24 +0200

    remove unused code

--- app/src/main/java/com/alexandre/bubblecheckbox/MainActivity.kt
+++ app/src/main/java/com/alexandre/bubblecheckbox/MainActivity.kt
@@ -13,29 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        // Setting up a spring animation to animate the view’s translationY property with the final
-        // spring position at 0.
-        val springAnim = SpringAnimation(hello_word, DynamicAnimation.TRANSLATION_Y, 0f)
-
-        root_view.setOnClickListener({
-
-        })
-
-        root_view.setOnTouchListener( { view, motionEvent ->
-            when (motionEvent.action){
-                MotionEvent.ACTION_DOWN -> {
-                    //hello_word.x = motionEvent.x
-                    //hello_word.y = motionEvent.y
-                }
-                MotionEvent.ACTION_UP -> {
-                    view.performClick()
-
-                }
-            }
-            return@setOnTouchListener true
-        })
     }
 }
